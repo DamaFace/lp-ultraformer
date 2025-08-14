6b810660aba0bbdb54df29ff254d4c6e5c4d62c9
DamaFace <marketingdamaface@gmail.com>
2025-08-14 13:27:48 -0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://n8n-n8n.i4khe5.easypanel.host/webhook/labios', {
+      const response = await fetch('https://n8n-n8n.i4khe5.easypanel.host/webhook/bioestimulador', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
