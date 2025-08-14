c5b317575d910ee29f64c2cc16902f21ba3059b6
DamaFace <marketingdamaface@gmail.com>
2025-08-14 16:20:47 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Carrossel from '@/components/Carrossel';
 import Depoimentos from '@/components/Depoimentos';
-import DepilacaoLaserInfo from '@/components/DepilacaoLaserInfo';
+import LaserLavieenInfo from '@/components/LaserLavieenInfo';
 import PorqueEscolher from '@/components/PorqueEscolher';
 import FAQ from '@/components/FAQ';
 import Footer from '@/components/Footer';
@@ -27,7 +27,7 @@ export default function Home() {
       <ChatWidget />
       <Carrossel />
       <Depoimentos />
-      <DepilacaoLaserInfo />
+      <LaserLavieenInfo />
       <PorqueEscolher />
       <FAQ />
       <Footer />
