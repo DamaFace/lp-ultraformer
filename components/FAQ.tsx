afad806d51aeff58e7e225c919266d10380fc2bc
DamaFace <marketingdamaface@gmail.com>
2025-08-14 17:39:27 -0300

    Update FAQ.tsx

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -24,7 +24,7 @@ const FAQ = () => {
             <span className="text-[#fa1571]"> Frequentes</span>
           </h2>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-            Esclarecemos as principais dúvidas sobre Laser Lavieen
+            Esclarecemos as principais dúvidas sobre Ultraformer
           </p>
         </div>
 
