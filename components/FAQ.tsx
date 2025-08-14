a68c6fa1fb7936abaa0bf01703f5535f82f2d9a7
DamaFace <marketingdamaface@gmail.com>
2025-08-14 17:05:27 -0300

    Update FAQ.tsx

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -24,7 +24,7 @@ const FAQ = () => {
             <span className="text-[#fa1571]"> Frequentes</span>
           </h2>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-            Esclarecemos as principais dúvidas sobre Harmonização de Glúteos
+            Esclarecemos as principais dúvidas sobre Laser Lavieen
           </p>
         </div>
 
